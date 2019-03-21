8eb4b224d3b324b5361f6247141a98991104963e
kumarvk <kumarvk>
2019-03-21 16:53:59 +0530

    Changes apply for gilded_rose and testcase fix

--- ruby/gilded_rose.rb
+++ ruby/gilded_rose.rb
@@ -7,24 +7,26 @@ class GildedRose
   def update_quality()
     @items.each do |item|
       if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
-        if item.quality > 0
+        if item.quality > 0  && item.quality <= 50
           if item.name != "Sulfuras, Hand of Ragnaros"
             item.quality = item.quality - 1
           end
+        elsif item.quality > 50 && item.name != "Sulfuras, Hand of Ragnaros"
+          item.quality = 50
+        elsif item.name != "Sulfuras, Hand of Ragnaros"
+          item.quality = item.quality - item.quality
+        elsif item.name == "Sulfuras, Hand of Ragnaros"
+          item.quality = 80
         end
       else
         if item.quality < 50
           item.quality = item.quality + 1
           if item.name == "Backstage passes to a TAFKAL80ETC concert"
-            if item.sell_in < 11
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
+            if item.sell_in < 11 && item.quality < 50
+              item.quality = item.quality + 1
             end
-            if item.sell_in < 6
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
+            if item.sell_in < 6 && item.quality < 50
+              item.quality = item.quality + 1
             end
           end
         end
--- ruby/gilded_rose_spec.rb
+++ ruby/gilded_rose_spec.rb
@@ -6,7 +6,61 @@ describe GildedRose do
     it "does not change the name" do
       items = [Item.new("foo", 0, 0)]
       GildedRose.new(items).update_quality()
-      expect(items[0].name).to eq "fixme"
+      expect(items[0].name).to eq "foo"
+    end
+
+    it "decrease item quality" do
+      items = [Item.new("foo", 0, 10)]
+      GildedRose.new(items).update_quality()
+      expect(items[0].quality).to  be < 10
+    end
+
+    it "item quality cannot be negative" do
+      items = [Item.new("foo", 0, -1)]
+      GildedRose.new(items).update_quality()
+      expect(items[0].quality).to  be >= 0
+    end
+
+    it "Aged Brie quality cannot decreases" do
+      items = [Item.new("Aged Brie", -1, 1)]
+      GildedRose.new(items).update_quality()
+      expect(items[0].quality).to  be > 1
+    end
+
+    it "item quality should be less or equal 50" do
+      items = [Item.new("bar", 1, 52)]
+      GildedRose.new(items).update_quality()
+      expect(items[0].quality).to  be <= 50
+    end
+
+    it "quality alway should be 80 for Sulfuras" do
+      items = [Item.new("Sulfuras, Hand of Ragnaros", 10, 60)]
+      GildedRose.new(items).update_quality()
+      expect(items[0].quality).to  eq 80
+    end
+
+    it "quality cannot change for Sulfuras" do
+      items = [Item.new("Sulfuras, Hand of Ragnaros", 10, 80)]
+      GildedRose.new(items).update_quality()
+      expect(items[0].quality).to  eq 80
+    end
+
+    it "increase quality by 3 if 6 or less day remain for Backstage" do
+      items = [Item.new("Backstage passes to a TAFKAL80ETC concert", 4, 1)]
+      GildedRose.new(items).update_quality()
+      expect(items[0].quality).to  be > 3
+    end
+
+    it "increase quality by 2 if 10 or less day remain for Backstage" do
+      items = [Item.new("Backstage passes to a TAFKAL80ETC concert", 10, 1)]
+      GildedRose.new(items).update_quality()
+      expect(items[0].quality).to  be > 2
+    end
+
+    it "quality should 0 for Backstage sell out days" do
+      items = [Item.new("Backstage passes to a TAFKAL80ETC concert", 0, 1)]
+      GildedRose.new(items).update_quality()
+      expect(items[0].quality).to  eq 0
     end
   end
 
